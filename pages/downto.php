f969450e63e255a4975692f34589c4f3ad07a15f
anthony1389 <phong92510@st.vimaru.edu.vn>
2025-05-19 08:31:04 +0700

    Update downto.php

--- pages/downto.php
+++ pages/downto.php
@@ -99,7 +99,8 @@ session_start();
 						<ul class="navbar-category-list">
 							<li class="navbar-category-item">
 							<?php
-										$conn = mysqli_connect("localhost", "root", "", "dacs2");
+										include("ketnoi.php");
+										$conn3 = $conn;
 										$sql = "SELECT * From danhmuc";
 										$ketqua = mysqli_query($conn,$sql);
 										while($row=mysqli_fetch_array($ketqua)){
@@ -181,7 +182,8 @@ session_start();
 							<ul class="category-list">
 								<li class="category-item category-item--active">
 								<?php
-										$conn = mysqli_connect("localhost", "root", "", "dacs2");
+										include("ketnoi.php");
+										$conn3 = $conn;
 										$sql = "SELECT * From danhmuc";
 										$ketqua = mysqli_query($conn,$sql);
 										while($row=mysqli_fetch_array($ketqua)){
@@ -231,7 +233,8 @@ session_start();
 						
                   
 		<?php
-			$conn =	mysqli_connect("localhost", "root", "", "dacs2");
+			include("ketnoi.php");
+										$conn3 = $conn;
 			$sql= "SELECT * FROM sanpham ORDER BY giasp DESC";
 			$ketqua = mysqli_query($conn, $sql);
 
