2e04af353e648b6ee80986950a19e05c6207b0a4
anthony1389 <phong92510@st.vimaru.edu.vn>
2025-05-19 08:43:59 +0700

    Update xoactdh.php

--- chitietdonhang/xoactdh.php
+++ chitietdonhang/xoactdh.php
@@ -1,7 +1,9 @@
 <?php
 		session_start();
+include("ketnoi.php");
+										$conn3 = $conn;
 		$id = $_GET['id'];
-		$conn =	mysqli_connect("localhost", "root", "", "dacs2");
+
 		$sql= "DELETE FROM chitietdonhang where id=$id ";
 		$ketqua = mysqli_query($conn, $sql);
 		header("location: ../chitietdonhang/quanlyctdh.php");
