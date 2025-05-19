3409431cf1586e255f94396585e26e97c629ac40
anthony1389 <phong92510@st.vimaru.edu.vn>
2025-05-19 08:44:50 +0700

    Update quanlybl.php

--- binhluan/quanlybl.php
+++ binhluan/quanlybl.php
@@ -100,7 +100,8 @@ session_start();
 						<ul class="navbar-category-list">
 							<li class="navbar-category-item">
 							<?php
-										$conn = mysqli_connect("localhost", "root", "", "dacs2");
+										include("ketnoi.php");
+										$conn3 = $conn;
 										$sql = "SELECT * From danhmuc";
 										$ketqua = mysqli_query($conn,$sql);
 										while($row=mysqli_fetch_array($ketqua)){
@@ -199,7 +200,8 @@ session_start();
 <tbody>
 <?php
 	
-	$conn =	mysqli_connect("localhost", "root", "", "dacs2");
+	include("ketnoi.php");
+	$conn3 = $conn;
 	$sql= "SELECT * FROM binhluan ";
 	$ketqua = mysqli_query($conn, $sql);
 	$stt = 1;
