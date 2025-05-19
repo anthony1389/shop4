931436e513300ca39bf4d6dc3d0dcefba002dc21
anthony1389 <phong92510@st.vimaru.edu.vn>
2025-05-19 07:36:18 +0700

    Create ketnoi.php

---
+++ binhluan/ketnoi.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "datashop.mysql.database.azure.com";
+$username = "sqladmin";
+$password = "phong218.";
+$dbname = "ban_hang";
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối không thành công: " . $conn->connect_error);
+}
+
+echo "";
+?>
