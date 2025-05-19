d7c7cf5becc8d7ea40a9fa68f80391daedfae871
anthony1389 <phong92510@st.vimaru.edu.vn>
2025-05-19 07:51:45 +0700

    Update xoatk.php

--- taikhoan/xoatk.php
+++ taikhoan/xoatk.php
@@ -1,7 +1,8 @@
-<?php
+<?php	
+		include("ketnoi.php");
 		session_start();
 		$id = $_GET['id'];
-		$conn =	mysqli_connect("localhost", "root", "", "dacs2");
+		$conn3 = $conn;
 		$sql= "DELETE FROM taikhoan where id=$id ";
 		$ketqua = mysqli_query($conn, $sql);
 		header("location: ../taikhoan/quanlytk.php");
