1ebcdcd748d2d3b78aa7862b5d8306847dce5401
anthony1389 <phong92510@st.vimaru.edu.vn>
2025-05-19 08:37:30 +0700

    Update xoabl.php

--- pages/xoabl.php
+++ pages/xoabl.php
@@ -1,7 +1,8 @@
 <?php
 		session_start();
 		$id = $_GET['id'];
-		$conn =	mysqli_connect("localhost", "root", "", "dacs2");
+		include("ketnoi.php");
+		$conn3 = $conn;
 		$sql= "DELETE FROM binhluan where id=$id ";
 		$ketqua = mysqli_query($conn, $sql);
 ?>
